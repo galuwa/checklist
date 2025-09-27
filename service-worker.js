3a20276e539b221f0eb9caefbb18dd5c8130bb49
galuwa <lukemillerpiano@gmail.com>
2025-09-26 19:24:00 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+
+const CACHE_NAME = "checklist-cache-v1";
+const FILES_TO_CACHE = [
+  "N89SF_Checklist.html",
+  "manifest.json"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
